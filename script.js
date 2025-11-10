07758c1124119440c7e7608a856ea154f1e3e019
Memtoz-dev <xiusmo4@gmail.com>
2025-11-10 22:45:01 +0000

    javascript major wip

---
+++ script.js
@@ -0,0 +1,5 @@
+const display = document.getElementById("display");
+const numInput = document.getElementsByClassName(".num-btn");
+const operatorInput = document.getElementsByClassName(".operator-btn");
+const equalsButton = document.getElementById("equals-btn");
+const clearButton = document.getElementById("clear-btn");
