f5b98af0919409cd5cea6369a2c6da3b3f7f158c
Memtoz-dev <xiusmo4@gmail.com>
2025-11-11 21:28:40 +0000

    Numbers append to display

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const display = document.getElementById("display");
-const numInput = document.getElementsByClassName(".num-btn");
-const operatorInput = document.getElementsByClassName(".operator-btn");
+const numInput = document.getElementsByClassName("num-btn");
+const add = document.getElementById("addition");
+const subtract = document.getElementById("subtraction");
+const divide = document.getElementById("division");
 const equalsButton = document.getElementById("equals-btn");
 const clearButton = document.getElementById("clear-btn");
+
+let calcStorage = "";
+let operator = "";
+
+for (let i = 0; i < numInput.length; i++){
+  numInput[i].addEventListener("click", function(){
+    parseInt(this.textContent);
+    calcStorage += this.textContent;
+    display.value += this.textContent;
+  });
+}
+
+clearButton.addEventListener("click", function(){
+  display.value = ("");
+});
+
+add.addEventListener("click", function(){
+    if (display.value == ("")){
+    display.value += ("");
+  }
+  else {
+    display.value += this.textContent;
+  }
+  
+});
+
+
