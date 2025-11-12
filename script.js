61cad9cd92e24a1e040c6247be341e3418c8a95e
Memtoz-dev <xiusmo4@gmail.com>
2025-11-12 23:00:04 +0000

    Setting up functions for arithmetic operations

--- script.js
+++ script.js
@@ -8,27 +8,57 @@ const clearButton = document.getElementById("clear-btn");
 
 let calcStorage = "";
 let operator = "";
+let num1 = 0;
+let num2 = 0;
 
-for (let i = 0; i < numInput.length; i++){
-  numInput[i].addEventListener("click", function(){
-    parseInt(this.textContent);
+for (let i = 0; i < numInput.length; i++) {
+  numInput[i].addEventListener("click", function () {
+
+    if (display.value == 0) {
+      display.value = "";
+      calcStorage = "";
+    }
+
+    parseFloat(this.textContent);
     calcStorage += this.textContent;
     display.value += this.textContent;
+    console.log(calcStorage);
+
+
   });
 }
 
-clearButton.addEventListener("click", function(){
-  display.value = ("");
+clearButton.addEventListener("click", function () {
+  calcStorage = "";
+  display.value = ("0");
 });
 
-add.addEventListener("click", function(){
-    if (display.value == ("")){
+add.addEventListener("click", function () {
+  if (display.value == ("")) {
     display.value += ("");
   }
   else {
     display.value += this.textContent;
   }
-  
+  if (num1 == 0){
+  num1 = calcStorage;
+  calcStorage = "";
+  console.log(num1);
+  console.log(calcStorage);
+  }
+
+});
+
+equalsButton.addEventListener("click", function(){
+  if (display.value == ("")){
+    display.value +=("");
+  }
+  else {
+    num2 = calcStorage;
+    calcStorage = "";
+
+    display.value = (parseFloat(num1)+ parseFloat(num2));
+  }
 });
 
 
